48c104e7ad63a5f3f0d6e532835ea7fad96b150c
ABoyd <phoenix239@users.noreply.github.com>
2023-01-28 03:26:58 -0800

    Changed fetch for all items list

--- EtsyFormatter.js
+++ EtsyFormatter.js
@@ -8,10 +8,11 @@ const { Server } = require('http');
 const SHOP_ID = 20692514;
 const sectionsUrl = `https://openapi.etsy.com/v3/application/shops/${SHOP_ID}/sections`;
 const x_api_key = '89t4onk893vp1xsgifqogfiv';
+// NOTE: Place Newest Listings First in Line!!
 const Earrings =
   '710574152,724432175,774820728,728749506,871073324,884997169,761770838,761771080,775644993,761769856,775644761,761769034,761769260,761769542,722125695,953480739,939528186,710573748,722128815,708268952,708350998,713481212,774820172,742610037,743724102,722209839';
 const Pendants =
-  '743722532,743721308,757589125,724435341,956905038,708275794,775642205,954834035,939530500,798578012,812459087,710575432,757587697,800313771,788707399,711518670,757590925,725378803,761766712,775642033,732939021,719079064,725380327,757592605,788707659,722133413,802100228';
+  '1303277148,1303274760,743722532,743721308,757589125,724435341,956905038,708275794,775642205,954834035,939530500,798578012,812459087,710575432,757587697,800313771,788707399,711518670,757590925,725378803,761766712,775642033,732939021,719079064,725380327,757592605,788707659,722133413,802100228';
 const Rings =
   '727334631,871060928,713479720,956902166,713477668,812452989,713478292,727336689,757595795';
 const Nose = '885005191';
@@ -105,7 +106,7 @@ const listingInfo = (item, index) => {
     processingMax: item.processing_max,
     hasVariations: item.has_variations,
     price: {
-      amount: item.price.amount / item.price.divisor,
+      amount: Math.round(item.price.amount / item.price.divisor),
       currency: item.price.currency_code,
     },
     images: imageInfo(item.images),
@@ -119,6 +120,7 @@ const separateData = (data, section) => {
   let rings = [];
   let nose = [];
   let sets = [];
+  let all = [];
 
   data.results.forEach((item, index) => {
     switch (item.shop_section_id) {
@@ -138,51 +140,42 @@ const separateData = (data, section) => {
         sets.push(listingInfo(item, index));
         break;
     }
-
-    switch (section) {
-      case 'earrings':
-        fs.writeFileSync(
-          'Listings/EtsyEarrings.json',
-          JSON.stringify(earrings)
-        );
-        break;
-      case 'pendants':
-        fs.writeFileSync(
-          'Listings/EtsyPendants.json',
-          JSON.stringify(pendants)
-        );
-        break;
-      case 'rings':
-        fs.writeFileSync('Listings/EtsyRings.json', JSON.stringify(rings));
-        break;
-      case 'nose':
-        fs.writeFileSync('Listings/EtsyNose.json', JSON.stringify(nose));
-        break;
-      case 'sets':
-        fs.writeFileSync('Listings/EtsySets.json', JSON.stringify(sets));
-        break;
-      case 'all':
-      default:
-        fs.writeFileSync(
-          'Listings/EtsyEarrings.json',
-          JSON.stringify(earrings)
-        );
-        fs.writeFileSync(
-          'Listings/EtsyPendants.json',
-          JSON.stringify(pendants)
-        );
-        fs.writeFileSync('Listings/EtsyRings.json', JSON.stringify(rings));
-        fs.writeFileSync('Listings/EtsyNose.json', JSON.stringify(nose));
-        fs.writeFileSync('Listings/EtsySets.json', JSON.stringify(sets));
-        break;
-    }
+    all.push(listingInfo(item, index));
   });
+
+  switch (section) {
+    case 'earrings':
+      fs.writeFileSync('Listings/EtsyEarrings.json', JSON.stringify(earrings));
+      break;
+    case 'pendants':
+      fs.writeFileSync('Listings/EtsyPendants.json', JSON.stringify(pendants));
+      break;
+    case 'rings':
+      fs.writeFileSync('Listings/EtsyRings.json', JSON.stringify(rings));
+      break;
+    case 'nose':
+      fs.writeFileSync('Listings/EtsyNose.json', JSON.stringify(nose));
+      break;
+    case 'sets':
+      fs.writeFileSync('Listings/EtsySets.json', JSON.stringify(sets));
+      break;
+    case 'all':
+    default:
+      fs.writeFileSync('Listings/EtsyEarrings.json', JSON.stringify(earrings));
+      fs.writeFileSync('Listings/EtsyPendants.json', JSON.stringify(pendants));
+      fs.writeFileSync('Listings/EtsyRings.json', JSON.stringify(rings));
+      fs.writeFileSync('Listings/EtsyNose.json', JSON.stringify(nose));
+      fs.writeFileSync('Listings/EtsySets.json', JSON.stringify(sets));
+      fs.writeFileSync('Listings/EtsyAll.json', JSON.stringify(all));
+      break;
+  }
 };
 
 app.get('/', async (req, res) => {
   let response;
   const operation = process.argv[2];
-  const listingSection = process.argv[3] ? process.argv[3] : 'all';
+  // const listingSection = process.argv[3] ? process.argv[3] : 'all';
+  const listingSection = 'all';
   console.log('Operation: ', operation);
   if (operation === 'listings') {
     console.log('Listing Section: ', listingSection);
@@ -196,50 +189,68 @@ app.get('/', async (req, res) => {
       );
       break;
     case 'listings':
-      switch (listingSection) {
-        case 'earrings':
-          response = await fetch(
-            getListingSectionUrl(Earrings), // Get Earrings
-            requestOptions
-          );
-          break;
-        case 'pendants':
-          response = await fetch(
-            getListingSectionUrl(Pendants), // Get Pendants
-            requestOptions
-          );
-          break;
-        case 'rings':
-          response = await fetch(
-            getListingSectionUrl(Rings), // Get Rings
-            requestOptions
-          );
-          break;
-        case 'nose':
-          response = await fetch(
-            getListingSectionUrl(Nose), // Get Nose
-            requestOptions
-          );
-          break;
-        case 'sets':
-          response = await fetch(
-            getListingSectionUrl(Sets), // Get Sets
-            requestOptions
-          );
-          break;
-        case 'all':
-        default:
-          response = await fetch(
-            getListingSectionUrl(AllListings), // Get Listings
-            requestOptions
-          );
-          break;
-      }
+      response = await fetch(
+        getListingSectionUrl(AllListings), // Get Listings
+        requestOptions
+      );
       break;
     default:
       console.log(`${operation} invalid, Please specify a valid command`);
       process.exit(1);
   }
+
+  // switch (operation) {
+  //   case 'sections':
+  //     response = await fetch(
+  //       sectionsUrl, // Get Sections
+  //       requestOptions
+  //     );
+  //     break;
+  //   case 'listings':
+  //     switch (listingSection) {
+  //       case 'earrings':
+  //         response = await fetch(
+  //           getListingSectionUrl(Earrings), // Get Earrings
+  //           requestOptions
+  //         );
+  //         break;
+  //       case 'pendants':
+  //         response = await fetch(
+  //           getListingSectionUrl(Pendants), // Get Pendants
+  //           requestOptions
+  //         );
+  //         break;
+  //       case 'rings':
+  //         response = await fetch(
+  //           getListingSectionUrl(Rings), // Get Rings
+  //           requestOptions
+  //         );
+  //         break;
+  //       case 'nose':
+  //         response = await fetch(
+  //           getListingSectionUrl(Nose), // Get Nose
+  //           requestOptions
+  //         );
+  //         break;
+  //       case 'sets':
+  //         response = await fetch(
+  //           getListingSectionUrl(Sets), // Get Sets
+  //           requestOptions
+  //         );
+  //         break;
+  //       case 'all':
+  //       default:
+  //         response = await fetch(
+  //           getListingSectionUrl(AllListings), // Get Listings
+  //           requestOptions
+  //         );
+  //         break;
+  //     }
+  //     break;
+  //   default:
+  //     console.log(`${operation} invalid, Please specify a valid command`);
+  //     process.exit(1);
+  // }
 
   if (response?.ok) {
     const data = await response.json();
@@ -275,6 +286,12 @@ app.listen(port, () => {
     });
 });
 
-// To Run:
+// To Run, pick one of the following commands:
 // node EtsyFormatter listings
+// node EtsyFormatter listings all
+// node EtsyFormatter listings earrings
+// node EtsyFormatter listings pendants
+// node EtsyFormatter listings rings
+// node EtsyFormatter listings nose
+// node EtsyFormatter listings sets
 // node EtsyFormatter sections
